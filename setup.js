1e53adab8331821c0cc9e153594871991454bf56
Anna <loukitzanna@gmail.com>
2018-04-03 16:10:49 -0700

    Upgrade to Babel 7 and newest AVA

--- setup.js
+++ setup.js
@@ -1,3 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import register from '@babel/register';
+
+register({ extensions: ['.ts', '.js', '.tsx', '.jsx']});
 configure({ adapter: new Adapter() });
